84e321c07f7a778f462040f17cf4780b4315a190
unknown <oheredia@exact.com.pe>
2020-10-12 14:02:33 -0500

    Termino correcciones Orlando

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,8 +5,6 @@ import { LoginProvider } from 'src/app/infrastructure/api/login.provider';
 import { UserDetails } from 'src/app/core/model/userdetails.model';
 import { Router, UrlSerializer } from '@angular/router';
 import { AppConfig } from 'src/app/app.config';
-import { catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
 import { NotifierService } from 'angular-notifier';
 
 @Component({
@@ -21,10 +19,11 @@ export class LoginComponent implements OnInit {
 
   constructor(private loginProvider: LoginProvider, private router: Router, private serializer: UrlSerializer, 
     private notifier: NotifierService) {
-    //this.loginUseCase = new LoginUseCase(loginProvider);
+     
   }
 
   ngOnInit(): void {
+    
     this.loginForm = new FormGroup({
       'usuario': new FormControl('', Validators.required), 
       'password': new FormControl('', Validators.required)
@@ -35,14 +34,16 @@ export class LoginComponent implements OnInit {
     if(value.usuario=="" || value.password==""){
       this.notifier.notify("warning", "Ingrese todos los datos solicitados");
     }else{
-      this.loginProvider.login(value.usuario, value.password).subscribe((userDetails: UserDetails) => {
+      this.loginProvider.login(value.usuario.trim(), value.password.trim()).subscribe((userDetails: UserDetails) => {
         const queryParams = {
           token: userDetails.getToken(),
           refresh_token: userDetails.getRefreshToken()
         }
         const tree = this.router.createUrlTree([], {queryParams});
-        const params = this.serializer.serialize(tree);
-        window.location.href = AppConfig.REDIRECT_URL + params;
+        const params = this.serializer.serialize(tree).split('?')[1];
+        value.usuario = '';
+        value.password = '';
+        window.location.href = AppConfig.REDIRECT_URL + '?' +  params;
   
       }, error => {
         console.log(error);
